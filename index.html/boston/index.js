dd018788e90f55ce86f2b906d690c6efe601d0b5
veerasakthivel2003 <veera212472@gmail.com>
2024-10-16 12:11:08 +0530

    Add files via upload

---
+++ index.html/boston/index.js
@@ -0,0 +1,25 @@
+import { boston } from "./boston.js";
+
+let data = boston.data;
+let topearners = [];
+
+for (let i = 0; i < data.length; i++) {
+  if (data[i][11] < 20000000) {
+    topearners.push({ name: data[i][8], salary: data[i][11] });
+  }
+}
+
+topearners.sort(function (a, b) {
+  return b.salary - a.salary;
+});
+
+topearners = topearners.slice(0, 5);
+
+let Earners = "<h1><u>Top salary:</u></h1>";
+for (let i = 0; i < topearners.length; i++) {
+  Earners += `<h2>Top salary ${i + 1}: ${topearners[i].name} - Salary: ${
+    topearners[i].salary
+  }</h2>`;
+}
+document.getElementById("app").innerHTML = Earners;
+console.log(topearners);
